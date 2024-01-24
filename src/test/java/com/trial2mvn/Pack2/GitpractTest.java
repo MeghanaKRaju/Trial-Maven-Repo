f31dfc879d25e829fd181e3564b907ea21f75216
Shailendra <Shailendra@DESKTOP-0U7543P>
2024-01-24 23:15:05 +0530

    Trial is running

---
+++ src/test/java/com/trial2mvn/Pack2/GitpractTest.java
@@ -0,0 +1,13 @@
+package com.trial2mvn.Pack2;
+
+import org.testng.annotations.Test;
+
+public class GitpractTest
+{
+	@Test
+	public void mvnTest1()
+	{
+		System.out.println("Trial 1 is running fine");
+	}
+
+}
